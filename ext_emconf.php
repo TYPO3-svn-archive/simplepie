eea79a4145ee936eeb94c9f5b7f6ee53aa3b66f8
leitnerg <leitnerg@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-22 14:22:58 +0000

    fixed #8424 Add plugin to Content Wizard
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simplepie/trunk@34756 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "simplepie".
 #
-# Auto generated 09-06-2010 15:04
+# Auto generated 22-06-2010 16:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,17 +11,17 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Simplepie',
-	'description' => 'RSS Plugin using Simplepie',
+	'title' => 'SimplePie',
+	'description' => 'Aggregates and displays various web feeds using SimplePie.',
 	'category' => '',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '1.0.0',
 	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'stable',
+	'state' => 'beta',
 	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.3.dev-4.3.99',
+			'typo3' => '4.3.dev-4.4.99',
 			'extbase' => '1.0.1-0.0.0',
 			'fluid' => '1.0.1-0.0.0',
 		),
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:33:"class.tx_simplepie_flexhelper.php";s:4:"0def";s:17:"ext_localconf.php";s:4:"86f8";s:14:"ext_tables.php";s:4:"6fb5";s:14:"ext_tables.sql";s:4:"ac3f";s:36:"Classes/Controller/RssController.php";s:4:"f64c";s:34:"Classes/Domain/Model/RssConfig.php";s:4:"c731";s:33:"Classes/Domain/Model/RssEntry.php";s:4:"2221";s:49:"Classes/Domain/Repository/RssConfigRepository.php";s:4:"0408";s:29:"Classes/Utility/Extension.php";s:4:"2050";s:41:"Configuration/FlexForms/flexform_list.xml";s:4:"1103";s:25:"Configuration/TCA/tca.php";s:4:"e80e";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"ee29";s:36:"Resources/Private/Libs/simplepie.inc";s:4:"2b65";s:42:"Resources/Private/Templates/Rss/index.html";s:4:"0c5f";}',
+	'_md5_values_when_last_written' => 'a:19:{s:33:"class.tx_simplepie_flexhelper.php";s:4:"0def";s:12:"ext_icon.gif";s:4:"6cf0";s:17:"ext_localconf.php";s:4:"86f8";s:14:"ext_tables.php";s:4:"f871";s:14:"ext_tables.sql";s:4:"ac3f";s:31:"icon_tx_simplepie_rssconfig.gif";s:4:"475a";s:36:"Classes/Controller/RssController.php";s:4:"7b3d";s:34:"Classes/Domain/Model/RssConfig.php";s:4:"eb2a";s:33:"Classes/Domain/Model/RssEntry.php";s:4:"95a2";s:49:"Classes/Domain/Repository/RssConfigRepository.php";s:4:"0408";s:29:"Classes/Utility/Extension.php";s:4:"2050";s:41:"Configuration/FlexForms/flexform_list.xml";s:4:"0cb7";s:25:"Configuration/TCA/tca.php";s:4:"e80e";s:65:"Resources/Private/Backend/class.tx_simplepie_feedlist_wizicon.php";s:4:"2b75";s:64:"Resources/Private/Backend/icon_tx_simplepie_feedlist_wizicon.gif";s:4:"70ca";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"910f";s:36:"Resources/Private/Libs/simplepie.inc";s:4:"3c6c";s:42:"Resources/Private/Templates/Rss/index.html";s:4:"d5d4";s:45:"Resources/Public/Javascript/simplepie_ajax.js";s:4:"a5ed";}',
 	'suggests' => array(
 	),
 );
--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,5 @@
 <?php
-If(!defined('TYPO3_MODE')) Die ('Access denied.');
+if(!defined('TYPO3_MODE')) Die ('Access denied.');
 
 $lPath = 'LLL:EXT:simplepie/Resources/Private/Language/locallang_db.xml';
 $ePath = t3lib_extMgm::extPath($_EXTKEY);
@@ -14,22 +14,26 @@ $TCA['tx_simplepie_domain_model_rssconfig'] = Array (
 		'delete' => 'deleted',
 		'enablecolumns' => Array ( 'disabled' => 'hidden' ),
 		'dynamicConfigFile' => $ePath . 'Configuration/TCA/tca.php',
-		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY)."icon_tx_simplepie_rssconfig.gif", 
+		'iconfile' => $ePath . 'icon_tx_simplepie_rssconfig.gif',
 	),
 );
 
-t3lib_extMgm::addStaticFile (
-	$_EXTKEY, 'Configuration/TypoScript', 'Simplepie' );
-	
-Tx_Extbase_Utility_Extension::registerPlugin (
-	$_EXTKEY, 'Pi1', 'RSS Simplepie Plugin' );
+t3lib_extMgm::addStaticFile ($_EXTKEY, 'Configuration/TypoScript', 'Simplepie');
 
-include_once(t3lib_extMgm::extPath($_EXTKEY).'class.tx_simplepie_flexhelper.php');
-	
+Tx_Extbase_Utility_Extension::registerPlugin ($_EXTKEY, 'Pi1', $lPath . ':feedlist.cewiz.title');
+
+include_once($ePath . 'class.tx_simplepie_flexhelper.php');
+
 $extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
 $pluginSignature = strtolower($extensionName) . '_pi1';
-	
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');	
+t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
+
+if (TYPO3_MODE == 'BE') {
+	// Adds a wizard icon to the content element wizard.
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_simplepie_feedlist_wizicon'] = $ePath . 'Resources/Private/Backend/class.tx_simplepie_feedlist_wizicon.php';
+}
+
 ?>
