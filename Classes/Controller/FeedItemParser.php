6cd32e9b5474b7b014ffcb81a0072bc8c4729789
leitnerg <leitnerg@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-29 12:13:13 +0000

    - added basic Flickr parser (#8525) - enhaced default feedItem template
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simplepie/trunk@35081 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/FeedItemParser.php
+++ Classes/Controller/FeedItemParser.php
@@ -2,26 +2,79 @@
 
 Class Tx_Simplepie_Controller_FeedController_FeedItemParser {
 
+	protected $title = '';
+	protected $date = '';
+	protected $author = '';
+	protected $copyright = '';
+	protected $description = '';
+	protected $permalink = '';
+	protected $content = '';
+	protected $items = '';
+	protected $feedImageUrl = '';
+	protected $feedTitle = '';
+	protected $timestamp = 0;
+	protected $type = 'unknown';
+
+	protected $feedItem;
+
+
 	/**
 	 * Parse a SimplePie item object and correct values
 	 *
 	 * $item	object	SimplePie item object
 	 */
 	Public Function parseObject($item) {
-		// echo '<h3>parseObject</h3><pre>';
-		// print_r($item->get_author());
 
-		// FeedEntry object
+		$this->feedItem = $item;
+
+		// set SiplePie values
+		$this->author = $item->get_author();
+		$this->title = $item->get_title();
+		$this->date = $item->get_date();
+		$this->copyright = $item->get_copyright();
+		$this->description = $item->get_description();
+		$this->permalink = $item->get_permalink();
+		$this->content = $item->get_content();
+		$this->timestamp = $item->get_date('U');
+		$this->type = $this->getItemType();
+
+		// do some basic cleanup
+		if ($this->content == $this->description) {
+			$this->content = false;
+		}
+		if ($this->description == $this->title || html_entity_decode($this->description) == $this->title) {
+			$this->description = false;
+		}
+
+		// special feed types
+		switch ($this->type) {
+			case 'facebook':
+				break;
+			case 'flickr':
+				$this->parseFlickrItem();
+				break;
+			case 'twitter':
+				break;
+			case 'youtube':
+				break;
+		}
+
+		// decode entities
+		$this->author->name = html_entity_decode($this->author->name);
+		$this->description = html_entity_decode($this->description);
+		$this->content = html_entity_decode($this->content);
+
+		// create FeedEntry object
 		$feedEntry = new Tx_Simplepie_Domain_Model_FeedEntry();
-		$feedEntry->setAuthor($item->get_author());
-		$feedEntry->setTitle($item->get_title());
-		$feedEntry->setDate($item->get_date());
-		$feedEntry->setCopyright($item->get_copyright());
-		$feedEntry->setDescription($item->get_description());
-		$feedEntry->setPermalink($item->get_permalink());
-		$feedEntry->setContent($item->get_content());
-		$feedEntry->setTimestamp($item->get_date('U'));
-		$feedEntry->setType($this->getItemType($item));
+		$feedEntry->setAuthor($this->author->name);
+		$feedEntry->setTitle($this->title);
+		$feedEntry->setDate($this->date);
+		$feedEntry->setCopyright($this->copyright);
+		$feedEntry->setDescription($this->description);
+		$feedEntry->setPermalink($this->permalink);
+		$feedEntry->setContent($this->content);
+		$feedEntry->setTimestamp($this->date);
+		$feedEntry->setType($this->type);
 
 		return $feedEntry;
 	}
@@ -29,10 +82,11 @@ Class Tx_Simplepie_Controller_FeedController_FeedItemParser {
 	/**
 	 * checks if a feed item comes from a known source as YouTube, Flickr & Co.
 	 */
-	Private function getItemType($item) {
+	Private function getItemType() {
 		$type = 'unknown';
 
-		$author = $item->get_item_tags('', 'author');
+		$author = $this->feedItem->get_item_tags('', 'author');
+		// Flickr
 		if (isset($author[0]['attribs']['urn:flickr:']['profile'])) {
 			$type = 'flickr';
 		}
@@ -40,15 +94,28 @@ Class Tx_Simplepie_Controller_FeedController_FeedItemParser {
 		return $type;
 	}
 
-	Private Function parseFlickrItem() {
+	Private Function parseFacebookItem() {
 		
+	}
+
+	/**
+	 * corrects Flickr feed items
+	 */
+	Private Function parseFlickrItem() {
+
+		// correct name/email: nobody@flickr.com ([Flickr Account Name])
+		if (preg_match('/^nobody@flickr\.com/', $this->author->email)) {
+			$this->author->name = preg_replace('/^([^(]*)\((.*)\)$/', "$2", $this->author->email);
+			$this->author->email = '';
+		}
+		// use <media:description> instead of <description>
+		$description = $this->feedItem->get_item_tags('http://search.yahoo.com/mrss/', 'description');
+		if (isset($description[0]['data']) && $description[0]['data'] != '') {
+			$this->description = $description[0]['data'];
+		}
 	}
 
 	Private Function parseTwitterItem() {
-		
-	}
-
-	Private Function parseFacebookItem() {
 		
 	}
 
