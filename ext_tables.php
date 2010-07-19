c0c4be9b4348b88d031e5a7ad967f6eec082a415
jochenlandvoigt <jochenlandvoigt@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-19 22:00:59 +0000

    Feature #8402
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simplepie/trunk@35828 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -27,7 +27,7 @@ include_once($ePath . 'class.tx_simplepie_flexhelper.php');
 $extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
 $pluginSignature = strtolower($extensionName) . '_pi1';
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/FeedList.xml');
 
